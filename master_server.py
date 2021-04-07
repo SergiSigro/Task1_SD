1864c742442894195a84bb93c0cddb85a23e1f7a
SergiSigro <sergi.sigro@estudiants.urv.cat>
2021-04-07 11:30:31 +0200

    Sergi: Preparando workers

--- master_server.py
+++ master_server.py
@@ -1,9 +1,14 @@
 import workers
 import time
+import redis
 
 if __name__ == "__main__":
+    tareas = redis.Redis(host='localhost', port=6379, db=0)
+    tareas.flushall()
+    tareas.lpush("inst", "Instruccion 1")
+    tareas.lpush("inst", "Instruccion 2")
+
     for i in range(5):
         workers.create_worker()
-        
     pass
 
--- workers.py
+++ workers.py
@@ -1,13 +1,26 @@
 from multiprocessing import Process
 import time
+import redis
 
 WORKERS = {}
 WORKER_ID = 0
 
+def work_to_do(task, tareas):
+    if not task:
+        return
+    else:
+        #Analizar la tarea
+        
+
 def start_worker(id):
+    keyCola="inst"
+    print('Worker: ', id)
+    tareas = redis.Redis(host='localhost', port=6379, db=0) #Creamos la conexión con la cola de tareas (servidor redis)
     while True:
-        print('Worker: ', id)
-        time.sleep(1)
+        #Comprobar si tenemos tareas
+        task=tareas.rpop(keyCola)
+        work_to_do(task, tareas)
+        time.sleep(2.5)
 
 
 def create_worker():
