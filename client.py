43ef202b22dedce18a6dfd576cda9f0d4d1e3987
AleixBertran <aleixbertran2@gmail.com>
2021-04-10 16:28:40 +0200

    Primeres versions del client i servidor fets amb grpc

---
+++ client.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+
+import random
+import logging
+import sys
+
+import grpc
+
+import MasterServer_pb2_grpc
+import MasterServer_pb2
+
+def genNumero(numero):
+    return MasterServer_pb2.numero(
+        num=int(numero)
+    )
+    
+def genInputFiles():
+    files=sys.argv[3]
+    if len(sys.argv) > 3:
+        for i in sys.argv[4:]:
+            files +=" "+i
+    files = files.translate({ord(c): None for c in "[,]"})
+    return MasterServer_pb2.input(fitxers=files)
+
+def create_worker(stub, num):
+    resultat = stub.CreateWorker(num)
+    print(resultat.resultat)
+
+def delete_worker(stub, num):
+    resultat = stub.DeleteWorker(num)
+    print(resultat.resultat)
+
+def list_workers(stub):
+    resultat = stub.ListWorkers()
+    print(resultat.resultat)
+
+def word_count(stub, files):
+    resultat = stub.GetResultatWC(files)
+    print(resultat.resultat)
+
+def count_words(stub, files):
+    resultat = stub.GetResultatCW(files)
+    print(resultat.resultat)
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = MasterServer_pb2_grpc.MasterServerStub(channel)
+        if sys.argv[1] == 'worker':
+            if sys.argv[2] == 'create':
+                if len(sys.argv) != 4:
+                    create_worker(stub, genNumero(1))
+                else:
+                    create_worker(stub, genNumero(sys.argv[4]))
+            elif sys.argv[2] == 'delete':
+                if len(sys.argv) != 4:
+                    delete_worker(stub, genNumero(1))
+                else:
+                    delete_worker(stub, genNumero(sys.argv[4]))
+            elif sys.argv[2] == 'list':
+                list_workers(stub)
+            else:
+                print("Pots crear un o més workers amb create.\nPots eliminar un o més workers amb delete.\nPots mostrar una llista dels workers amb list.")
+        elif sys.argv[1] == 'job':
+            if sys.argv[2] == 'run-wordcount':
+                if len(sys.argv) != 4:
+                    print("Necesites introduir un fitxer de entrada.")
+                else:
+                    word_count(stub, genInputFiles())
+            elif sys.argv[2] == 'run-countwords':
+                if len(sys.argv) != 4:
+                    print("Necesites introduir un fitxer de entrada.")
+                else:
+                    count_words(stub, genInputFiles())
+            else:
+                print("Per comptar el nombre total de paraules en diferents fitxers utilitza run-countwords.\nPer comptar el nombre d'ocurrències de cada paraula utilitza run-wordcount.")
+        else:
+            print("Introduint worker com a primer paràmetre pots gestionar els workers.\nIntroduint job pots enviar una petició per a un treball.\nRecorda: per a enviar una petició de treball necessites tenir com a mínim un worker.")
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
--- master_server.py
+++ master_server.py
@@ -1,54 +1,81 @@
-import workers
+from concurrent import futures
+
 import time
+import math
+import logging
+
+
+import grpc
+
+import MasterServer_pb2
+import MasterServer_pb2_grpc
+
 import redis
+import workers
 
-if __name__ == "__main__":
+
+class MasterServerServicer(MasterServer_pb2_grpc.MasterServerServicer):
+    #Functions
+    def __init__(self):
+        self.JOB_ID = 0 #numero del trabajo == key para obtener el resultado de la operacion
+        self.NUM_WORKERS = 0 #numero de workers
+        self.r = redis.Redis(host='localhost', port=6379, db=0)  #crear servidor redis
+        self.r.flushall()    #eliminamos todo lo que estuviese en el redis anteriormente
+        self.keyCola = "inst"    #key de la cola de instrucciones
+
+    def GetResultatCW(self, request, context):
+        files = request.fitxers.split()
+        self.JOB_ID += 1
+        numFiles = len(files)
+        for i in files:
+            tupla = (self.JOB_ID, "CountingWords", i, numFiles)
+            self.r.lpush(self.keyCola, tupla)
+        while self.r.lindex(self.JOB_ID, 0) != "done":
+            pass
+        resultat = self.r.lindex(self.JOB_ID, -1)
+        self.r.delete(self.JOB_ID)
+        return MasterServer_pb2.resultat(resultat="Total de paraules contades en els fitxers: " + str(files) + " = " + str(resultat))
     
-    #crear servidor redis
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.flushall()    #eliminamos todo lo que estuviese en el redis anteriormente
+    def GetResultatWC(self, request, context):
 
-    keyCola = "inst"    #key de la cola de instrucciones
-    JOB_ID=0    #numero del trabajo == key para obtener el resultado de la operacion
+        files = request.fitxers.split()
+        self.JOB_ID += 1
+        numFiles = len(files)
+        for i in files:
+            tupla = (self.JOB_ID, "WordCount", i, numFiles)
+            self.r.lpush(self.keyCola, tupla)
+        while self.r.lindex(self.JOB_ID, 0) != "done":
+            pass
+        resultat = self.r.lindex(self.JOB_ID, -1)
+        self.r.delete(self.JOB_ID)
+        return MasterServer_pb2.resultat(resultat="Resultat de WordCount dels fitxers: " + str(files) + "\n" + resultat)
 
-    workerManagement = ["createWorker", "deleteWorker", "listWorkers"]  #lista de comandos relacionados con la administracion de los workers
-    programs = ["CountingWords", "WordCount"]   #lista de programas 
-    while True:
-        #recibir instrucciones del usuario
-        print("Introduce un comando:")
-        instruccion = input()
-        instruccion = instruccion.split()
-        
-        
-        #si instruccion esta relacionada con la administracion de los workers
-        if instruccion[0] in workerManagement:
-            if instruccion[0] == workerManagement[0]:
-                workers.createWorker(r)
-            elif instruccion[0] == workerManagement[1]:
-                workers.deleteWorker()
-            else:
-                workers.listWorkers()
-        
+    def CreateWorker(self, request, context):
+        i = 0
+        while i < request.num:
+            workers.createWorker(self.r)
+            self.NUM_WORKERS += 1
+            i += 1
+        return MasterServer_pb2.resultat(resultat="Workers creats correctament.")
 
-        #si instruccion es uno de los programas
-        elif instruccion[0] in programs:
-            JOB_ID += 1
-            numFile=1
-            while numFile < len(instruccion):
-                tupla = (JOB_ID, instruccion[0], instruccion[numFile], len(instruccion)-1)
-                r.lpush(keyCola, tupla)
-                numFile += 1
-            while r.lindex(JOB_ID, -1) != "done":
-                pass
-            print(r.lpop(JOB_ID))
-        
-         
-        #si instruccion no es valida
-        else:
-            print("Operacion incorrecta")
-        
-        
-        
-        
-    pass
+    def DeleteWorker(self, request, context):
 
+        if request.num > self.NUM_WORKERS:
+            return MasterServer_pb2.resultat(resultat="No es poden eliminar aquella cuantitat de workers.\nNumero de workers: "+str(self.NUM_WORKERS))
+        i = 0
+        while i < request.num:
+            workers.deleteWorker()
+            self.NUM_WORKERS -= 1
+            i += 1
+        return MasterServer_pb2.resultat(resultat="Workers eliminats correctament.")
+
+    def ListWorkers(self, request, context):
+        return MasterServer_pb2.resultat(resultat=workers.listWorkers())
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    MasterServer_pb2_grpc.add_MasterServerServicer_to_server(
+        MasterServerServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
